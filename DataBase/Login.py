5eb5b419145f54374cf6697e5a652155e5a02a6f
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2020-04-02 22:05:56 +0530

    Update Login.py

--- DataBase/Login.py
+++ DataBase/Login.py
@@ -13,12 +13,28 @@ def read_sql(data):
     df = pd.read_sql_query("SELECT * FROM password",con)
     return df
 
+def update(data):
+    new_user = input("Enter the username for your profile:  ")
+    new_pass = input("Enter the password for your username:  ")
+    new_user = str(new_user)
+    new_pass = str(new_pass)
+    connect = sqlite3.connect(data)
+    curser = connect.cursor()
+    curser.execute('''INSERT INTO  password (USERNAME,PASSWORD) values(?,?)''',(new_user,new_pass))
+    connect.commit()
+    new_database = pd.read_sql_query("SELECT * FROM password",connect)
+    new_Items_list = list(new_database["USERNAME"])
+    new_Quantity_list = list(new_database["PASSWORD"])
+    new_dictionary = {new_Items_list[i]:new_Quantity_list[i] for i in range(len(new_Quantity_list))}
+    return new_dictionary
+
 def main():
     database = "  "  #Put the name of your database here
     dataframe = read_sql(database)
     Auth = read_excel(dataframe)
     
     for i in Auth:
+      flag = -1
       login = input("enter your login id:  ")
       if login in Auth:
         print("username found")
@@ -27,7 +43,16 @@ def main():
         break
       else:
         print("username not found")
-    print("Too Many Tries Logging in")   
+        break
+        
+    if(flag == -1):
+      confirm = input("Please Register to view Database(y/n):  ")  
+      if(confirm == 'y'):
+         Auth= update(database)
+         print("Now please Refresh the Page and try logging in Again")
+         flag = -2
+      else:
+         print("Database access denied") 
     
     while(flag ==1):
        x = Auth.get(login)
