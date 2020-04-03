9ae1d36cf23221caffaefa8ca6de74669be614e3
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2020-04-03 19:26:24 +0530

    Update Login.py

--- DataBase/Login.py
+++ DataBase/Login.py
@@ -1,16 +1,18 @@
+import numpy
+import numpy as np
 import sqlite3
 import pandas as pd
-import numpy as np
+
 
 def read_excel(df):
     Items_list = list(df["USERNAME"])
     Quantity_list = list(df["PASSWORD"])
     dictionary = {Items_list[i]:Quantity_list[i] for i in range(len(Quantity_list))}
-    return dictionary
+    return Items_list,Quantity_list,dictionary
 
 def read_sql(data):
     con = sqlite3.connect(data)
-    df = pd.read_sql_query("SELECT * FROM password",con)
+    df = pd.read_sql_query("SELECT * FROM Personal",con)
     return df
 
 def update(data):
@@ -20,9 +22,9 @@ def update(data):
     new_pass = str(new_pass)
     connect = sqlite3.connect(data)
     curser = connect.cursor()
-    curser.execute('''INSERT INTO  password (USERNAME,PASSWORD) values(?,?)''',(new_user,new_pass))
+    curser.execute('''INSERT INTO  Personal (USERNAME,PASSWORD) values(?,?)''',(new_user,new_pass))
     connect.commit()
-    new_database = pd.read_sql_query("SELECT * FROM password",connect)
+    new_database = pd.read_sql_query("SELECT * FROM Personal",connect)
     new_Items_list = list(new_database["USERNAME"])
     new_Quantity_list = list(new_database["PASSWORD"])
     new_dictionary = {new_Items_list[i]:new_Quantity_list[i] for i in range(len(new_Quantity_list))}
@@ -43,24 +45,54 @@ def new_login(Dict):
 
      return login
 
-def main():
-    database = "  "  #Put the name of your database here
-    dataframe = read_sql(database)
-    Auth = read_excel(dataframe)
-    
-    for i in Auth:
-      flag = -1
-      login = input("enter your login id:  ")
-      if login in Auth:
-        print("username found")
-        login = str(login)
-        flag = 1
-        break
-      else:
-        print("username not found")
-        break
+def medical_data(data,Dict):
+    ID = input("enter your current username:  ")
+    if ID in Dict: 
+       counter = input('''update your list in this format "item:quantity" seperated by comma :    ''')
+       connect = sqlite3.connect(data)
+       curser = connect.cursor()
+       curser.execute('''Update Personal set DETAILS = (?) where USERNAME = (?)''',(counter,ID))
+       connect.commit()
+
+def personal_details(data,Dict):
+    ID = input("enter your current username:  ")
+    if ID in Dict:
+      phone_number = input("Enter Your Phone Number:  ")
+      address = input("Enter Your Address:    ")
+      phone_number= str(phone_number)
+      address  = str(address)
+      connect = sqlite3.connect(data)
+      curser = connect.cursor()
+      #curser.execute('''REPLACE INTO  Personal (ADDRESS,PHONE) values(?,?)''',(address,phone_number))
+      curser.execute('''Update Personal set ADDRESS = (?) where USERNAME = (?)''',(address,ID))
+      curser.execute('''Update Personal set PHONE = (?) where USERNAME = (?)''',(phone_number,ID))
+      connect.commit()
+      outcome = "S"
+      return outcome
+    else:
+        print("Wrong Username")
+        outcome = "F"
+        return outcome
         
-   if(flag == -1):
+    
+
+def main():
+    database = "Final_Login.db"
+    dataframe = read_sql(database)
+    username_list, password_list,Auth = read_excel(dataframe)
+    flag = 0
+    
+    if(flag==0):
+       login = input("enter your login id:  ")
+       if login in Auth:
+         print("username found")
+         login = str(login)
+         flag = 1
+       else:
+         print("username not found")
+         flag = -1
+
+    if(flag == -1):
       confirm = input("Please Register to view Database(y/n):  ")  
       if(confirm == 'y'):
          Auth= update(database)
@@ -80,7 +112,7 @@ def main():
              if(reset_flag ==5):
                  print("Error:To many tries Logging in , Refresh the Page and Try Again")
                  break
-                    
+             
     while(flag ==1):
        x = Auth.get(login)
        password = input("enter your password:  ")
@@ -91,9 +123,29 @@ def main():
          break
        else:
          print("Wrong Password")
-    
+
     if(flag==2):
-        print("login successful")
+       confirm = input("login successful.Have you uploaded your phone number and Address?(y/n):   ")
+       if(confirm=='y'):
+          flag = 3
+           
+       else:
+           o = personal_details(database,Auth)
+           print(o)
+           if(o=="S"):
+              flag = 3
+           else:
+               print("phone number and address not updated,Refresh the page and Try again")
+
+    if(flag==3):
+        print("profile updated")
+        update = input("Do you want to Upload or update your medical List(y/n):   ")
+        if(update == 'y'):
+            medical_data(database,Auth)
+            flag = 4
+            print("Medical Data is Successfully Updated")
+        else:
+            print("Medical Data Not updated")
            
            
 if __name__ == "__main__":
